f3b16db2a7d8699c2e3f880c3e4ce29a69e91301
Bharath369 <marturubharath369@gmail.com>
2024-07-08 13:22:35 -0400

    updated

--- amazon_products_streamlit.py
+++ amazon_products_streamlit.py
@@ -42,8 +42,8 @@ st.markdown(
         position: absolute;
         top: -10px;
         right: 0px;
-        width: 30px;  # Adjust the width as needed
-        height: 30px;  # Adjust the height as needed
+        width: 10px;  # Adjust the width as needed
+        height: 10px;  # Adjust the height as needed
     }}
     </style>
     <div class="logo">
